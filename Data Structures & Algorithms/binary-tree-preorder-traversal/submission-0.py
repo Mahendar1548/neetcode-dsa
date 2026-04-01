17f65c5d5dba0c20d8ab7ab0331780eb403cb502
Mahendar1548 <badumahendra@gmail.com>
2026-04-01 18:02:20 +0530

    Add: binary-tree-preorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+
+        def dfs(node):
+            if not node:
+                return
+            ans.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ans
